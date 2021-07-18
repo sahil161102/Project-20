c82f42f9e3f67fa0ca01cd3e92ad6117aae81c73
sahil161102 <85942526+sahil161102@users.noreply.github.com>
2021-07-18 16:15:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var bullet, wall;
+var width = 0, weight,speed;
+
+
+function setup() {
+  createCanvas(1600,400);
+  width = random(22,83);
+  wall=createSprite(1200,200,width, height/2);
+  wall.shapeColor="white";
+  bullet=createSprite(200,200,20, 20);
+  bullet.shapeColor="white";
+  speed = random(223,321);
+  weight = random(30,52);
+  bullet.velocityX = speed;
+}
+
+function draw() {
+  background("black");  
+
+
+  
+  var deformation = 0.5*speed*speed*weight/(width*width*width);
+  if(isTouching(wall,bullet)){
+  if(deformation>10){
+    bullet.shapeColor="Red";
+  }
+  else if(deformation<=10){
+    bullet.shapeColor="green";
+  }
+  bullet.velocityX =0;
+}
+  drawSprites();
+}
