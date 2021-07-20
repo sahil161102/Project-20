79090beda147433d6d84dc1160ba43996d7b6705
sahil161102 <85942526+sahil161102@users.noreply.github.com>
2021-07-20 17:26:59 +0530

    Update MyLibrary.js

--- MyLibrary.js
+++ MyLibrary.js
@@ -1,8 +1,8 @@
-function isTouching(movingRect,fixedRect){
-    if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+function hasCollided(movingRect,fixedRect){
+    var movingRectRightEdge = movingRect.x + movingRect.width;
+    var fixedRectLeftEdge = fixedRect.x;
+    
+    if (movingRectRightEdge>= fixedRectLeftEdge) {
       
       return true;
     }
